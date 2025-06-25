3f260db3f0c0a46003c483c4728531af254b59e8
Raghav-don <raghavendra0417k@gmail.com>
2025-06-25 11:02:28 +0530

    scratch update

---
+++ app.js
@@ -0,0 +1,9 @@
+function setLightTheme() {
+    document.body.style.backgroundColor = "#ffffff";
+    document.body.style.color = "#000000";
+}
+
+function setDarkTheme() {
+    document.body.style.backgroundColor = "#121212";
+    document.body.style.color = "#ffffff";
+}
